d078a403228009f2fd91a649a8cf52b164cfa4a2
Vinícius Bittencourt <vinisataides@gmail.com>
2022-07-21 15:13:01 -0300

    Tryout

--- src/pages/tablet/index.js
+++ src/pages/tablet/index.js
@@ -10,8 +10,8 @@ export default function hoje() {
       <div className="container">
         <div className="modal">
             <div className="modal-header">
-            <h6><FiAlertCircle size={32} className="alertIcon"></FiAlertCircle>Título da mensagem</h6>
-            <FiX size={32} className="exit"></FiX>
+            <h6><FiAlertCircle size={24} className="alertIcon"></FiAlertCircle>Título da mensagem</h6>
+            <FiX size={24} className="exit"></FiX>
             </div>
             <p>Descrição da ação que o usuário está tomando.</p>
             <div className="btns">
