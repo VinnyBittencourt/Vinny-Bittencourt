c8f1cbd5819690acd25e7be9958a0de5c7af52d2
Vinícius Bittencourt <vinisataides@gmail.com>
2022-07-29 16:03:42 -0300

    RF

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
 import Old from "./pages/oldversion";
 import Tablet from "./pages/tablet"
+import Rf from "./pages/RF"
 
 
 export default function Routes() {
@@ -13,6 +14,7 @@ export default function Routes() {
                 <Route path='/' exact component={Home} />
                 <Route path='/oldversion'  component={Old} />
                 <Route path='/tablet'  component={Tablet} />
+                <Route path='/rf'  component={Rf} />
             </Switch>
         </BrowserRouter>
     );
---
+++ src/pages/RF/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiAlertCircle, FiX } from "react-icons/fi";
+
+import "./style.css";
+
+export default function hoje() {
+  return (
+    <>
+      <div className="rf">
+        <h1>Login</h1>
+        <h5>Login</h5>
+        <p>Aperte Enter</p>
+
+        <div className="box-inp">
+          <input type="text" placeholder="Nome" className="inputzera" />
+          <input type="password" placeholder="Senha" className="inputzera" />
+        </div>
+
+        <a href="#" className="btn-rf">Concluir</a>
+      </div>
+    </>
+  );
+}
