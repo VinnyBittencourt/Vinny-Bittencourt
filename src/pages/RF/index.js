509717102eb7a445f426734696ff2db1e98af332
Vinícius Bittencourt <vinisataides@gmail.com>
2023-05-09 15:39:46 -0300

    Testando tela mobile no arquivo do RF

--- src/pages/RF/index.js
+++ src/pages/RF/index.js
@@ -1,25 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FiAlertCircle, FiX } from "react-icons/fi";
+import { FiAlertCircle, FiX, FiArrowLeft } from "react-icons/fi";
 
 import "./style.css";
 
 export default function hoje() {
   return (
     <>
-    
-      <div className="rf">
-        <h1>Login</h1>
-        <h5>Login</h5>
-        <p>Aperte Enter</p>
-
-        <div className="box-inp">
-          <input type="text" placeholder="Nome" className="inputzera" />
-          <input type="password" placeholder="Senha" className="inputzera" />
+      <div className="mobile">
+        <div className="header">
+          <FiArrowLeft size={20}></FiArrowLeft>
+          <h2>Informar Lacre</h2>
         </div>
+        <div className="body">
 
-        <a href="#" className="btn-rf">Concluir</a>
-        <div className="caixa01"></div>
+          <div className="body_group">
+            <div className="disclaimer">
+              <FiAlertCircle size={16}></FiAlertCircle>
+              <p>Lacre o baú antes de informar o código do lacre</p>
+            </div>
+
+            <div className="info">
+              <h6>Informações do veículo:</h6>
+              <div className="group_info">
+                <div className="info__line">
+                  <p>Modelo:</p>
+                  <p className="bold">Toco 16</p>
+                </div>
+                <div className="info__line">
+                  <p>Placa:</p>
+                  <p className="bold">ABC12D3</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="positions">
+              <h6>Posições:</h6>
+              <div className="group_card">
+                <div className="card">
+                  <p>Traseira</p>
+                  <Link to="#">Informar</Link>
+                </div>
+                <div className="card">
+                  <p>Lateral 1</p>
+                  <Link to="#">Informar</Link>
+                </div>
+                <div className="card">
+                  <p>Lateral 2</p>
+                  <Link to="#">Informar</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Link to="#" className="btn">Finalizar</Link>
+        </div>
       </div>
     </>
   );
